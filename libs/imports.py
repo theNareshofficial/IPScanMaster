d06566bcad53a265bcb19341fbcbf09b012ed32e
theNareshofficial <thenareshoffcial@gmail.com>
2024-07-11 14:30:25 +0530

    Added(src.ports/tools.banner)

--- libs/imports.py
+++ libs/imports.py
@@ -1,9 +1,10 @@
 
-from src import ping
 from src.ping import *
 from src.waf import *
 from src.ipinfo import *
 from src.whois import *
 from src.reverse import *
+from src.ports import *
 from tools.clrscr import *
 from tools.inet import *
+from tools.banner import *

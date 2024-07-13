b9e3f48ec00355c54529822902d5863dc90a1a81
theNareshofficial <thenareshoffcial@gmail.com>
2024-07-13 14:13:50 +0530

    Ipchg

--- libs/imports.py
+++ libs/imports.py
@@ -5,6 +5,7 @@ from src.ipinfo import *
 from src.whois import *
 from src.reverse import *
 from src.ports import *
+from src.IPchg import *
 from tools.clrscr import *
 from tools.inet import *
 from tools.banner import *

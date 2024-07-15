4b225f64470624c7a2b8a9ffa6acc827f17918f7
theNareshofficial <thenareshoffcial@gmail.com>
2024-07-15 11:30:52 +0530

    Ansi code

---
+++ tools/color.py
@@ -0,0 +1,18 @@
+#TextStyle:
+RESET = '\033[0m'
+BOLD = '\033[1m'
+ITALIC = '\033[3m'
+UNDERLINE = '\033[4m'
+BLINK = '\033[5m'
+REVERSE = '\033[7m'
+HIDDEN = '\033[8m'
+
+#ForegroundColor:
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'

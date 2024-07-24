311a737c94b71a8846c37d6281a7717d5118abde
theNarshofficial <thenareshoffcial@gmail.com>
2024-07-24 17:35:23 +0530

    Bug fixed, color updated

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-from libs.imports import *
+from libs import *
 
 clrscr()
 banner()
 
+uIP = input(f"{BRIGHT_GREEN}[+] Enter Your DNS or IPv4 : {BRIGHT_CYAN}").replace("https://", "").replace("http://", "")
 
-uIP = input(f"{GREEN}[+] Enter Your DNS or IPv4 : {CYAN}").replace("https://", "").replace("http://", "")
-ip_history(ip=uIP)
-is_valid_ip_or_dns(uIP)
+ip_history(ip=uIP)      # It's store in your history file
+validate_ip_dns(uIP)
 
 def connection():
         global uIP
@@ -16,26 +16,27 @@ def connection():
         try:
                 if inet_conn() == 0:
 
-                        print(f"\n{YELLOW}[+]---------- Checking Your Original IP ----------[+]{RESET}{GREEN}\n")
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- Checking Your Original IP ----------[+]{RESET}{BRIGHT_GREEN}\n")
                         reverseIP(ip=uIP)
-                        print(f"\n{YELLOW}[+]---------- Checking Your IP Reachable ----------[+]{GREEN}\n")
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- Checking Your IP Reachable ----------[+]{BRIGHT_GREEN}\n")
                         ip_Reachable(ip=uIP)
-                        print(f"\n{YELLOW}[+]---------- Checking WAF(Web Application Firewall) ----------[+]{GREEN}\n")
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- Checking WAF(Web Application Firewall) ----------[+]{BRIGHT_GREEN}\n")
                         check_waf(ip=uIP)
-                        uIP = ip_chg(ip=uIP)
-                        print(f"\n{YELLOW}[+]---------- IP Information ----------[{GREEN}+]")
-                        ipinfo(ip=uIP)
-                        print(f"\n{YELLOW}[+]---------- Checking Open Port's ----------[+]{GREEN}\n")
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- Checking Sub-Domain's ----------[+]{BRIGHT_GREEN}\n")
+                        enumerate_subdomain(domain=uIP)
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- Checking Open Port's ----------[+]{BRIGHT_GREEN}\n")
                         scan_ports(ip=uIP)
-                        print(f"\n{YELLOW}[+]---------- IP WHOIS ----------[+]{GREEN}\n")
+                        uIP = ip_chg(ip=uIP)
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- IP Information ----------[{BRIGHT_GREEN}+]")
+                        ipinfo(ip=uIP)
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- IP WHOIS ----------[+]{BRIGHT_GREEN}\n")
                         check_whois(ip=uIP)
-                        print(f"\n{YELLOW}[+]---------- Completed ----------[+]{GREEN}\n")
+                        print(f"\n{BRIGHT_MAGENTA}[+]---------- Completed ----------[+]{BRIGHT_GREEN}\n")
                 else:
                         exit(None)
         
-        except Exception as e:
-                print(f"{RED}Error : {e}")
-
+        except Exception as conn_error:
+                print(f"{RED}Error : {conn_error}")
         except KeyboardInterrupt:
                 pass
 

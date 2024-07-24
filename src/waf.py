defc29e3b080077814614d8e222e8b12d361e923
theNarshofficial <thenareshoffcial@gmail.com>
2024-07-24 17:31:55 +0530

    Bug Fixed

--- src/waf.py
+++ src/waf.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import wafw00f
 import wafw00f.main
 from urllib.parse import urlparse
+from tools.color import *
 
 def check_waf(ip):
 
@@ -14,12 +15,13 @@ def check_waf(ip):
                     results = waf.identwaf()
 
                     if results:
-                                print(f"WAF is detected {ip}")
+                                print(f"{BRIGHT_MAGENTA}[+] {BRIGHT_CYAN}WAF is detected {ip}")
                                 for waf_name in results:
-                                        print(f" - {waf_name}")
+                                        print(f" {BRIGHT_MAGENTA}- {BRIGHT_CYAN}{waf_name}")
                     else:
-                                print(f"No WAF is detected!!!")
-
+                                print(f" {BRIGHT_MAGENTA} [+] {BRIGHT_RED} No WAF is detected!!!")
+            except Exception as waf_error:
+                    print(waf_error)    
             except KeyboardInterrupt:
                     pass
 

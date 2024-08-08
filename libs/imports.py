ea91bcc69ff1d100901a73c07670ea6055e1bf69
theNareshofficial <thenareshoffcial@gmail.com>
2024-08-08 20:26:05 +0530

    Bug fixed

--- libs/imports.py
+++ libs/imports.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python3
 
-from src.ping import *
-from src.waf import *
-from src.api.ipinfo import *
-from src.whois import *
-from src.reverse import *
-from src.ports import *
-from src.IPchg import *
-from src.sublister import *
-from tools.clrscr import *
-from tools.inet import *
-from tools.banner import *
-from tools.color import *
-from tools.ip_history import *
+try:
+                  from src.ping import *
+                  from src.waf import *
+                  from src.api.ipinfo import *
+                  from src.whois import *
+                  from src.reverse import *
+                  from src.ports import *
+                  from src.IPchg import *
+                  from src.sublister import *
+                  from tools.clrscr import *
+                  from tools.inet import *
+                  from tools.banner import *
+                  from tools.color import *
+                  from tools.ip_history import *
+
+except ModuleNotFoundError:
+        print(" Module Not found ERROR")
+        exit()
+except Exception as import_error:
+        print(f" [!] {import_error}, path={__file__}")

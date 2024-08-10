e39e238bcf751e22cbae77e22664cd722c8c573a
theNareshofficial <thenareshoffcial@gmail.com>
2024-08-10 14:26:58 +0530

    Config file

---
+++ tools/core/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+version      = "IPScanMaster : v2.01"
+short_name   = "IPScanMaster🔍"
+description  = "...A Tool for Gathering Detailed Information about IPs and Domains..."
+Author       = "Author  : Naresh"
+Github       = "Github  : https://github.com/theNareshofficial"
+Youtube      = "Youtube : https://www.youtube.com/@nareshtechweb930"

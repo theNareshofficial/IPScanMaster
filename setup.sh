592a6bc28839f184cafd58fd3000a0ab6b69a898
theNareshofficial <thenareshoffcial@gmail.com>
2024-08-14 15:26:28 +0530

    Updated global_access

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ echo """
           |  Github  : https://github.com/theNareshofficial
           |  Youtube : https://www.youtube.com/@nareshtechweb930    
 
-                            IPScanMaster : v1.12
+                            IPScanMaster : v2.02
     ...A Tool for Gathering Detailed Information about IPs and Domains...
 """
 

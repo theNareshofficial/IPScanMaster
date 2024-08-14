c7d570006bda56f3660c93131b478228947d127a
theNareshofficial <thenareshoffcial@gmail.com>
2024-08-14 15:10:26 +0530

    Updated global_access

--- setup.sh
+++ setup.sh
@@ -54,28 +54,20 @@ check_root() {
 
 check_inet() {
     if ping -c 1 8.8.8.8 &>/dev/null; then
-        echo "[+]---------- Internet Connection Successful ----------[+]"
+        echo "[+]---------->> Internet Connection Successful"
         return 0
     else
-        echo "[+]---------- Internet Connection Failed ----------[+]"
+        echo "[!]---------->> Internet Connection Failed"
         return 1
     fi
 }
 
-check_update() {
-    echo "[+]---------- Updating ----------[+]"
-    sudo apt update -y &>/dev/null
-    echo "[+]---------- Upgrading ----------[+]"
-    sudo apt upgrade -y &>/dev/null
-    echo "[+]---------- Update Completed ----------[+]"
-}
-
 check_python() {
     if command_exists python3; then
-        echo "[+]---------- Python Found ----------[+]"
+        echo "[+]---------->> Python Found"
         return 0
     else
-        echo "[+]---------- Python3 Not Found ----------[+]"
+        echo "[!]---------->> Python3 Not Found"
         echo "Python3 Installing..."
         sudo apt install python3 -y &>/dev/null
         echo "Python3 Installation Completed"
@@ -85,10 +77,10 @@ check_python() {
 
 check_git() {
     if command_exists git; then
-        echo "[+]---------- Git Found ----------[+]"
+        echo "[+]---------->> Git Found"
         return 0
     else
-        echo "[+]---------- Git Not Found ----------[+]"
+        echo "[!]---------->> Git Not Found"
         echo "Git Installing..."
         sudo apt install git -y &>/dev/null
         echo "Git Installation Completed"
@@ -98,28 +90,35 @@ check_git() {
 
 check_wafw00f() {
     if command_exists wafw00f; then
-        echo "[+]---------- Wafw00f Found ----------[+]"
+        echo "[+]---------->> Wafw00f Found"
     else
-        echo "[+]---------- Wafw00f Not Found ----------[+]"
+        echo "[!]---------->> Wafw00f Not Found"
         echo "Wafw00f Installing..."
         sudo apt install wafw00f -y &>/dev/null
         echo "Wafw00f Installation Completed"
     fi
 }
 
+global_access() {
+    chmod +x main.py
+    sudo ln -sf $(pwd)/main.py /usr/local/bin/ipscanmaster
+    echo "[+] IPScanMaster has been installed. You can now use the 'ipscanmaster' command from anywhere."
+
+}
+
 install_requirements() {
-    echo "[+]---------- Installing Python requirements ----------[+]"
+    echo "[+]---------->> Installing Python requirements"
     pip install -r requirements.txt &>/dev/null
-    echo "Python requirements installation completed"
+    echo "[+] Python requirements installation completed."
 }
 
 
 if check_root && check_inet; then
-    check_update
     check_python
     check_git
     check_wafw00f
     install_requirements
+    global_access
     exit 0
 else
     exit 1

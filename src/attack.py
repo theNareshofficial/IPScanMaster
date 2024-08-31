46007fe2bead8e9eb8b9916ad1fd97f40ca40dbc
theNareshofficial <thenareshoffcial@gmail.com>
2024-08-31 22:03:42 +0530

    Attack Mode

---
+++ src/attack.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import subprocess
+import datetime
+from tools.color import *
+from src.ports import *
+
+class Attacking:
+
+    def __init__(self, IP):
+        self.IP = IP
+
+    def run_commands(self):
+        try:
+
+            dt = datetime.datetime.now().strftime("%d:%B:%Y_%H:%M:%S")
+            path = (f"{self.IP}_{dt}")
+
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- Open Port Scan ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            enumerate_subdomain(domain=self.IP)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- SubDomain Enumeration ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            subprocess.run(f"sudo subfinder -d {self.IP} -o output/{path}/subdomains.txt", shell=True, check=True)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- HTTPX-Toolkit ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            subprocess.run(f"sudo httpx-toolkit -l output/{path}/subdomains.txt -o output/{path}/https.txt", shell=True, check=True)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- WAFW00F Check ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            subprocess.run(f"sudo wafw00f -i output/{path}/https.txt -o output/{path}/waf.txt", shell=True, check=True)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- NMAP Whois-Domain ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            subprocess.run(f"sudo nmap --script whois-domain.nse {self.IP} -oN output/{path}/whois_Domain.txt", shell=True)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- NMAP Whois-IP ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            subprocess.run(f"sudo nmap {self.IP} --script whois-ip -o output/{path}/whois_IP.txt", shell=True, check=True)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- NMAP Vulners ----------[+]{RESET}{BRIGHT_GREEN}\n")
+            subprocess.run(f"sudo nmap -sV --script vulners {self.IP} -oN output/{path}/Vulners.txt", shell=True, check=True)
+            print(f"\n{BRIGHT_MAGENTA}[+]---------- Completed ----------[+]{BRIGHT_GREEN}\n")
+
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_CYAN} Saved output/{path}/subdomains.txt")
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_CYAN} Saved output/{path}/https.txt")
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_CYAN} Saved output/{path}/waf.txt")
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_CYAN} Saved output/{path}/whois_IP.txt")
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_CYAN} Saved output/{path}/whois_Domain.txt")
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_CYAN} Saved output/{path}/Vulners.txt")
+
+        except subprocess.CalledProcessError as e:
+            print(f"{BRIGHT_MAGENTA}[+]{BRIGHT_RED} Error executing command: {e}")
+
+
+if __name__ == "__main__":
+    attcker = Attacking()
+    attcker.run_commands()

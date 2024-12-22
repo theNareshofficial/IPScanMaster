68803144041e019ed79850c5cead5c0778de5c60
theNareshofficial <thenareshoffcial@gmail.com>
2024-12-22 10:12:59 +0530

    version v2.08

--- tools/core/config.py
+++ tools/core/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-version      = "IPScanMaster : v2.07"
+version      = "IPScanMaster : v2.08"
 short_name   = "IPScanMaster🔍"
-description  = "...A Tool for Gathering Detailed Information about IPs and Domains..."
+description  = "...A Tool for Gathering Detaild Information about IPs and Domains..."
 Author       = "Author  : Naresh"
 Github       = "Github  : https://github.com/theNareshofficial"
 Youtube      = "Youtube : https://www.youtube.com/@nareshtechweb930"

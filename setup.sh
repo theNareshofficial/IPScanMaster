5cc267fe7e3980c7213b3c11c719e3177cd34f35
theNareshofficial <thenareshoffcial@gmail.com>
2024-12-22 10:13:45 +0530

    v2.08

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ echo """
           |  Github  : https://github.com/theNareshofficial
           |  Youtube : https://www.youtube.com/@nareshtechweb930    
 
-                            IPScanMaster : v2.07
+                            IPScanMaster : v2.08
     ...A Tool for Gathering Detailed Information about IPs and Domains...
 """
 

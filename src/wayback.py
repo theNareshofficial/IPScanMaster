78150c55b668a52dc23e337fe918888feb8eb060
theNareshofficial <thenareshoffcial@gmail.com>
2024-12-26 10:43:55 +0530

    theme updated

--- src/wayback.py
+++ src/wayback.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+from tools.color import *
 
 class Wayback:
     def __init__(self, url, output_path=str(None)):
@@ -16,25 +17,24 @@ class Wayback:
 
             if response.ok:
                 data = response.json()
+                
                 if data:
-                    print("Archived Versions:")
+                    print(f"{BRIGHT_GREEN}Archived Versions:")
                     results = []
                     for entry in data[1:]:  # Skip the header row
                         archive_url = f"https://web.archive.org/web/{entry[1]}/{entry[2]}"
-                        print(f"Timestamp: {entry[1]}, Archive URL: {archive_url}")
+                        print(f"{BRIGHT_MAGENTA}Timestamp: {entry[1]}, Archive URL: {BRIGHT_CYAN}{archive_url}")
                         results.append({"timestamp": entry[1], "archive_url": archive_url})
                     
                     # Save the results to the specified file
                     with open(self.output_path, "w") as f:
                         json.dump(results, f, indent=4)
-                    
-            #         print(f"\nResults saved to {self.output_path}")
                 else:
-                    print("No archived versions found.")
+                    print(f"{BRIGHT_RED}No archived versions found.")
             else:
-                print("Failed to fetch data.")
+                print("{BRIGHT_RED}Failed to fetch data.")
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
+            print(f"{BRIGHT_RED}An error occurred: {e}")
         except IOError as e:
-            print(f"An error occurred while writing to file: {e}")
+            print(f"{BRIGHT_RED}An error occurred while writing to file: {e}")

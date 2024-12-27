9cdd67252da54230a80c50b1a720f744c4b3675a
theNareshofficial <thenareshoffcial@gmail.com>
2024-12-27 11:36:45 +0530

    dirb updated

--- src/dir.py
+++ src/dir.py
@@ -7,7 +7,7 @@ class DirSearch:
         def search(self, ip=None):
                 self.ip = ip
                 try:
-                        subprocess.run(f"sudo dirsearch -u {self.ip} -w injection/dirb_small.txt", shell=True, check=True)
+                        subprocess.run(f"sudo dirsearch -u {self.ip} -w injection/dirb/dirb_small.txt", shell=True, check=True)
                 except subprocess.SubprocessError as dir_error:
                         print(f"DirSearch : {dir_error}")
 

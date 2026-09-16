228deb5057ee27af581ea628d089d435af2e45a8
theNareshofficial <thenareshoffcial@gmail.com>
2026-09-16 17:41:22 +0530

    Bug fixed 'DateTime'

--- tools/ip_history.py
+++ tools/ip_history.py
@@ -5,8 +5,7 @@ import datetime
 def ip_history(ip):
             
             try:
-                 date_string = "15-07-2024 : 14:30:45"
-                 dt = datetime.datetime.strptime(date_string, "%d-%m-%Y : %H:%M:%S")
+                 dt = datetime.datetime.now().strftime("%d-%m-%Y : %H:%M:%S")
                  with open("history", "a") as file:
                        file.write(f"{ip} {dt}\n")
                        print("[+] IP Saved in history file...")
